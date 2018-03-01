c32cf90db1777c3583cf5b74187ef0b57123a95c
irvin <irvinumundo@gmail.com>
2018-02-28 23:54:26 -0600

    Mas comentarios porque no los modifique en el repo D:

--- Factory Method/src/Main.java
+++ Factory Method/src/Main.java
@@ -1,4 +1,5 @@
-
+/*
+		 * Este patron es una manera común de crear clases y objetos sin exponer explícitamente la lógica de la creación, esto se hace a traves de HumanFactory que detecta el tipo de Clase y crea el tipo de clase. Asi mismo el tipo de clase detectado sobreescribe los métodos de las clases de las cuales hereda, por ejemplo en este caso de Human por parte de Male y Female*/
 public class Main {
 	public static void main(String [] args) {
 		HumanFactory humanFactory = new HumanFactory();
--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    //En este patron lo que se hace es que sea hace que el constructor para crear una clase sea privado y solo se pueda "crear" objetos instanciandolos, dado que el constructor no es visible para el main
 	public static void main(String[] args) {
 		SinglePerro singlePerro = SinglePerro.getInstance();
 		singlePerro.showMessage();
